7bbd1a276436887bddbac2aac3c8d093859dc3a9
Katie Koski <KKoski@abstraktmg.com>
2019-11-13 12:21:59 -0600

    added variables

--- exercises/part1-2.ts
+++ exercises/part1-2.ts
@@ -2,7 +2,11 @@
 // https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
 
 // Part 1: Add the 5 variables here
-
+let spacecraftName: string = "Determination";
+let speedMph: number = 17500;
+let kilometersToMars: number = 225000000;
+let kilometersToTheMoon: number = 384400;
+let milesPerKilometer: number = 0.621;
 
 
 
